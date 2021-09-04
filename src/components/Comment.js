4a6dc0b341b8779b02a29b525620e590d12d2cfb
Luan Zwang <luanzwng@gmail.com>
2021-09-04 16:40:15 -0300

    Building components.

---
+++ src/components/Comment.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Comment.css';
+
+const Comment = () => (
+    <div className="Comment">
+        <h1>Hello! I'm a component.</h1>
+    </div>
+)
+
+export default Comment
---
+++ src/components/FragmentedComment.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Comment.css';
+
+//JSX
+const FragmentedComment = () => (
+    <>
+        <div>Hello!</div>
+        <div>I'm a fragmented component.</div>
+    </>
+);
+
+export default FragmentedComment
